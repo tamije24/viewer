089df7cd46909f428d78f6029a286bf02b7214ec
Taufiq Zaidong <taufiqzaidong@gmail.com>
2025-04-15 17:21:52 +0800

    commit default port for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '10.249.53.47',  // Set the host to your desired IP
+    port: 8000,            // Set the port
+    strictPort: true,      // Ensures Vite only uses this port
+  }
+});
